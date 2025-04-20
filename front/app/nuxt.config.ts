7f7311aa0c590f8da84192a37fcc84be026bfc49
Your Name <you@example.com>
2025-04-20 12:53:45 +0200

    config ok with nitro static preset

--- front/app/nuxt.config.ts
+++ front/app/nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
   ssr: false,
+  nitro: {
+    preset: 'static'
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_API_BASE || 'http://localhost:8000',
