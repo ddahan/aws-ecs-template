39f1c2453f548efbcf697e492e8b153efe7f8e67
Your Name <you@example.com>
2025-04-20 16:51:25 +0200

    change allow_origins for production

--- front/app/nuxt.config.ts
+++ front/app/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_API_BASE || 'http://localhost:8000',
+      apiBase: process.env.NUXT_API_BASE || 'http://localhost:8000', // will work for local because NUXT_API_BASE is not defined
     },
   },
 })
